6672e2b8d8369f9d93b674bd6d4fd73d573cf80f
SuminBae97 <steavee@o.cnu.ac.kr>
2022-12-12 21:05:48 +0900

    9095 dp 복습

--- boj/boj_9095.py
+++ boj/boj_9095.py
@@ -1,15 +1,13 @@
-import sys
+
 t = int(input())
 dp = [0]*(12)
-dp[1]=1
-dp[2]=2
-dp[3]=4
-
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
 
 for i in range(t):
     n = int(input())
-
-    for i in range(4,n+1):
-        dp[i] = dp[i-2]+dp[i-1]+dp[i-3]
-
-    print(dp[n])
+    for j in range(4,n+1):
+        dp[j] = dp[j-1] + dp[j-2] + dp[j-3]
+    print(dp[n])    
+    
