c7612e3545dda42b98ce4a2bb6d2f333e30eb761
SuminBae97 <steavee@o.cnu.ac.kr>
2022-12-17 16:49:56 +0900

    백준 11725 복습

--- boj/boj_11725.py
+++ boj/boj_11725.py
@@ -2,27 +2,22 @@ import sys
 sys.setrecursionlimit(10**6)
 n = int(input())
 
-graph = [[] for _ in range(n+1)]
+tree = [[] for _ in range(n+1)]
+parent = [0]*(n+1)
 visited = [False]*(n+1)
-parent = [0 for _ in range(n+1)]
 
 for _ in range(n-1):
     a,b = map(int,sys.stdin.readline().split())
-    graph[a].append(b)
-    graph[b].append(a)
+    tree[a].append(b)
+    tree[b].append(a)
 
-
-
-
-def dfs(start):
-    visited[start]=True
-    for node in graph[start]:
+def dfs(x):
+    visited[x]=True
+    for node in tree[x]:
         if visited[node]==False:
-            parent[node]=start
+            parent[node]= x
             dfs(node)
+
 dfs(1)
 for i in range(2,n+1):
     print(parent[i])
-    
-
-
