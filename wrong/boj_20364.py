deb85e7664ae439b162f2bf9f1703380a125e721
SuminBae97 <steavee@o.cnu.ac.kr>
2022-12-18 10:25:54 +0900

    20364 이진트리 복습

--- wrong/boj_20364.py
+++ wrong/boj_20364.py
@@ -1,18 +1,23 @@
-n, q = map(int, input().split())
-ducks = [int(input())for _ in range(q)]
-visited = set()
+import sys
+n,q = map(int,input().split())
+num = []
+vis = set()
+for _ in range(q):
+    a = int(sys.stdin.readline().rstrip())
+    num.append(a)
 
-def dfs(val):
-    ans = 0 
-    duck = val
-    while duck > 0:
-        if duck in visited:
+def dfs(x):
+    ans = 0
+    duck = x
+    while duck>0:
+        if duck in vis:
             ans = duck
         duck = duck//2
-    
-    if ans == 0:
-        visited.add(val)
+
+    if ans==0:
+        vis.add(x)
     print(ans)
 
-for i in ducks:
+
+for i in num:
     dfs(i)
