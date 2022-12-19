e6d9f64b5dec8e337f480b5ea5bab97039140cb4
SuminBae97 <steavee@o.cnu.ac.kr>
2022-12-19 16:45:03 +0900

    boj 2407 파스칼 방법

---
+++ boj/boj_2407.py
@@ -0,0 +1,18 @@
+import sys
+import math
+
+n,m = map(int,sys.stdin.readline().split())
+dp = [[0 for i in range(101)] for j in range(101)]
+
+for i in range(1,101):
+    dp[i][0] = 1
+    dp[i][i] = 1
+
+for i in range(2,101):
+    for j in range(1,i):
+        dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+
+print(dp[n][m])
+# up = math.factorial(n)
+# down = math.factorial(n-m) * math.factorial(m)
+# print(up//down)
