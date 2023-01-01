32678b26df02290a70bc03580bfd0c3a6aabe257
SuminBae97 <steavee@o.cnu.ac.kr>
2023-01-01 20:42:48 +0900

    백준 15900 복습

--- boj/boj_15900.py
+++ boj/boj_15900.py
@@ -1,9 +1,10 @@
 import sys
 sys.setrecursionlimit(10**5)
 n = int(input())
-tree =[[] for _ in range(n+1)]
+tree = [[] for _ in range(n+1)]
 visited = [False]*(n+1)
-distance = [0]*(n+1)
+
+dist = [0]*(n+1)
 
 for _ in range(n-1):
     a,b = map(int,sys.stdin.readline().split())
@@ -12,22 +13,15 @@ for _ in range(n-1):
 
 def dfs(root):
     visited[root]=True
-    for node in tree[root]:
-        if visited[node]==False:
-            distance[node] = distance[root]+1
-            dfs(node)
-
-leaf_node =[]
-
+    for val in tree[root]:
+        if visited[val]==False:
+            dist[val]= dist[root] + 1
+            dfs(val)
+dfs(1)
+answer = 0
 for i in range(2,len(tree)):
     if len(tree[i])==1:
-        leaf_node.append(i)
-
-dfs(1)
-
-answer = 0
-for val in leaf_node:
-    answer+=distance[val]
+        answer+=dist[i]
 
 if answer%2==0:
     print("No")
