72b61db06adeb1847e77f639a1b78bbf0a058b8a
SuminBae97 <steavee@o.cnu.ac.kr>
2023-01-02 19:57:42 +0900

    hash 복습

--- wrong/programmers_롤케이크자르기.py
+++ wrong/programmers_롤케이크자르기.py
@@ -1,21 +1,20 @@
 from collections import Counter
 
 def solution(topping):
-    mine = Counter(topping)
-    answer=  0
-    other = {}
+    ct = Counter(topping)
+    others ={}
+    answer = 0
     for i in range(len(topping)):
-        if topping[i] in other:
-            other[topping[i]]+=1
+        if topping[i] in others:
+            others[topping[i]]+=1
         else:
-            other[topping[i]]=1
+            others[topping[i]] = 1
 
-        mine[topping[i]]-=1
+        ct[topping[i]]-=1
 
-        if mine[topping[i]]==0:
-            del mine[topping[i]]
+        if ct[topping[i]]==0:
+            del ct[topping[i]]
 
-
-        if len(mine)==len(other):
+        if len(others)== len(ct):
             answer+=1
     return answer
