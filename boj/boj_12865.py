932170b351b173abd0e8fc60fce56b8a7232599f
SuminBae97 <steavee@o.cnu.ac.kr>
2023-01-09 22:30:42 +0900

    knapsack 복습

--- boj/boj_12865.py
+++ boj/boj_12865.py
@@ -1,20 +1,19 @@
 import sys
+bags =[[0,0]]
 n,k = map(int,sys.stdin.readline().split())
-bag = [[0,0]]
 dp = [[0]*(k+1) for _ in range(n+1)]
-
 
 for _ in range(n):
     w,v = map(int,sys.stdin.readline().split())
-    bag.append([w,v])
+    bags.append([w,v])
 
 for i in range(1,n+1):
     for j in range(1,k+1):
-        w = bag[i][0]
-        v = bag[i][1]
+        w = bags[i][0]
+        v = bags[i][1]
 
         if j<w:
             dp[i][j] = dp[i-1][j]
         else:
-            dp[i][j] = max(dp[i-1][j-w]+v, dp[i-1][j])
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-w]+v)
 print(dp[n][k])
