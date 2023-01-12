76be46a4b8b5b0c551850fbdf216b492b0a56efc
SuminBae97 <steavee@o.cnu.ac.kr>
2023-01-12 23:28:46 +0900

    피곤해서 그냥 이전에 푼거 복습

--- boj/boj_14940.py
+++ boj/boj_14940.py
@@ -1,10 +1,7 @@
 import sys
 from collections import deque
-
 n,m = map(int,sys.stdin.readline().split())
-graph = []
-visited = [[False]*(m) for _ in range(n)]
-
+graph =[]
 dx = [1,-1,0,0]
 dy = [0,0,1,-1]
 
@@ -13,43 +10,45 @@ for _ in range(n):
     tmp = list(map(int,sys.stdin.readline().split()))
     graph.append(tmp)
 
-dest_loc = None
+answer = [[-1]*(m) for _ in range(n)]
+visited = [[False]*m for _ in range(n)]
+start_x = 0
+start_y = 0
+
 for i in range(n):
     for j in range(m):
         if graph[i][j]==2:
-            dest_loc = [i,j]
-            break
-
-
-ans = [[-1]*(m) for _ in range(n)]
-for i in range(n):
-    for j in range(m):
-        if graph[i][j]==0:
-            ans[i][j]=0
+            start_x = i
+            start_y = j
+            answer[i][j]=0
+        elif graph[i][j]==0:
+            answer[i][j]=0
 
 
 def bfs(x,y):
     q = deque([(x,y)])
     visited[x][y]=True
-
+    
     while q:
         x,y = q.popleft()
         for i in range(4):
-            nx = x+dx[i]
-            ny = y+dy[i]
-
+            nx = x+ dx[i]
+            ny = y+ dy[i]
             if nx<0 or nx>=n or ny<0 or ny>=m:
                 continue
 
             if graph[nx][ny]==1 and visited[nx][ny]==False:
-                ans[nx][ny] = ans[x][y]+1
-                visited[nx][ny]=True
                 q.append([nx,ny])
-ans[dest_loc[0]][dest_loc[1]] = 0
-bfs(dest_loc[0],dest_loc[1])
+                answer[nx][ny] = answer[x][y]+1
+                visited[nx][ny]=True
 
+bfs(start_x,start_y)
 
 for i in range(n):
     for j in range(m):
-        print(ans[i][j],end=' ')
+        print(answer[i][j],end=' ')
     print()
+
+
+
+
